16d8e892141a914e8c3e14ca5eda74e5ba065285
Aezazsiddiqui <muhammadaezazsiddiqui@gmail.com>
2026-06-01 15:21:29 +0500

    Update 01-variables.js

--- 01-basic/01-variables.js
+++ 01-basic/01-variables.js
@@ -4,5 +4,10 @@ var accountPassword = "20001117"
 accountCity = Karachi
 
 accountId = 200020
+accountEmail ="Saad@gmail.com"
+accountPassword = "20001128"
+accountCity = Lahore
 
 console.log(accountId);
+
+console.table([accountId ,accountEmail ,accountPassword ,accountCity]);
